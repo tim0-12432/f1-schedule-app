332e3ea6164560df3cfdbae29cbfccd4490a5f9a
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2022-08-06 10:20:45 +0200

    started own widget

---
+++ app/src/main/java/de/tim0_12432/f1_schedule_app/ui/schedule/ScheduleWidgetProvider.java
@@ -0,0 +1,19 @@
+package de.tim0_12432.f1_schedule_app.ui.schedule;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.os.Bundle;
+
+public class ScheduleWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+}
