0b684bcd3703589488740991d0525c008cff1269
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2022-08-08 18:09:02 +0200

    widget start

--- app/src/main/java/de/tim0_12432/f1_schedule_app/ui/schedule/ScheduleWidgetProvider.java
+++ app/src/main/java/de/tim0_12432/f1_schedule_app/ui/schedule/ScheduleWidgetProvider.java
@@ -4,16 +4,27 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.RemoteViews;
+
+import de.tim0_12432.f1_schedule_app.R;
 
 public class ScheduleWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_schedule);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_schedule);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 }
