cfd09f03f722f8df681d03b29d9ffa15a75b8112
Timo <79634593+tim0-12432@users.noreply.github.com>
2022-08-10 18:09:08 +0200

    custom history (#21)

---
+++ docs/preact.config.js
@@ -0,0 +1,11 @@
+export default (config, env, helpers) => {
+  config.output.publicPath = '/f1-schedule-app/';
+
+  config.devServer.devMiddleware.publicPath = "/f1-schedule-app/";
+
+  config.plugins.push(
+    new helpers.webpack.DefinePlugin({
+      'process.env.PUBLIC_PATH': JSON.stringify(config.output.publicPath || '/')
+    })
+  );
+};
