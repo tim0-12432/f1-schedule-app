e29493c615cb9678563629800c7bfd582a54a6a9
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2023-05-11 08:59:53 +0200

    fix unit tests

--- app/src/test/java/de/tim0_12432/f1_schedule_app/utility/SpeedTest.java
+++ app/src/test/java/de/tim0_12432/f1_schedule_app/utility/SpeedTest.java
@@ -10,7 +10,7 @@ public class SpeedTest {
         assertEquals("0.0", Speed.convertMphToKmh(""));
         assertEquals("0.0", Speed.convertMphToKmh(null));
         assertEquals("0.0", Speed.convertMphToKmh("0.0"));
-        assertEquals("1.61", Speed.convertMphToKmh("1.0"));
+        assertEquals("1.6", Speed.convertMphToKmh("1.0"));
         assertEquals("16.1", Speed.convertMphToKmh("10.0"));
         assertEquals("80.5", Speed.convertMphToKmh("50.0"));
         assertEquals("161.0", Speed.convertMphToKmh("100.0"));
@@ -22,7 +22,7 @@ public class SpeedTest {
         assertEquals("0.0", Speed.convertKmhToMph(""));
         assertEquals("0.0", Speed.convertKmhToMph(null));
         assertEquals("0.0", Speed.convertKmhToMph("0.0"));
-        assertEquals("0.62", Speed.convertKmhToMph("1.0"));
+        assertEquals("0.6", Speed.convertKmhToMph("1.0"));
         assertEquals("6.2", Speed.convertKmhToMph("10.0"));
         assertEquals("31.0", Speed.convertKmhToMph("50.0"));
         assertEquals("62.0", Speed.convertKmhToMph("100.0"));
